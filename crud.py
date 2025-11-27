5f5992f1bc80870e94f1541e12718577de19f14d
Vlad Blotenko <blosenkovlad@gmail.com>
2025-11-27 20:34:05 +0100

    Commit Crud Improve

--- crud.py
+++ crud.py
@@ -1,22 +1,44 @@
 from datetime import date
 from db import get_session
 from models import Driver, Vehicle, Assignment
+from sqlalchemy.orm import joinedload
+
 
 def list_drivers():
     with get_session() as s:
-        return s.query(Driver).order_by(Driver.full_name).all()
+        return (
+            s.query(Driver)
+            .order_by(Driver.full_name)
+            .all()
+        )
+
 
 def get_assignments_for_driver(name, d=None):
     with get_session() as s:
-        q = s.query(Assignment).join(Driver).filter(Driver.full_name == name)
+        q = (
+            s.query(Assignment)
+            .options(
+                joinedload(Assignment.driver),
+                joinedload(Assignment.vehicle)
+            )
+            .join(Driver)
+            .filter(Driver.full_name == name)
+        )
+
         if d:
             q = q.filter(Assignment.work_date == d)
+
         return q.order_by(Assignment.work_date).all()
+
 
 def get_assignments_for_date(d):
     with get_session() as s:
         return (
             s.query(Assignment)
+            .options(
+                joinedload(Assignment.driver),
+                joinedload(Assignment.vehicle)
+            )
             .filter(Assignment.work_date == d)
             .order_by(Assignment.driver_id)
             .all()
